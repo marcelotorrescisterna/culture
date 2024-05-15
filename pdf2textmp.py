92bd91d801436caef1e68f06ace24a92cc459d26
marcelotorrescisterna <69370786+marcelotorrescisterna@users.noreply.github.com>
2024-05-14 22:24:03 -0400

    Add files via upload

--- pdf2textmp.py
+++ pdf2textmp.py
@@ -8,15 +8,10 @@ from textcleaningutils import *
 import shutil as sh
 from multiprocessing import Pool, Manager
 import time
-import logging
-
-# Configure logging
-logging.basicConfig(filename='processing_log.log', level=logging.INFO, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def process_document(index, total, doc, INPUT_FOLDER, OUTPUT_FOLDER, BAD_PDF_FOLDER, corpus_list, ids_list, id2doc_list):
     try:
-        logging.info(f"PROCESSING FILE : {doc} ({index + 1}/{total})")
+        print(f"PROCESSING FILE : {doc} ({index + 1}/{total})")
         reader = PdfReader(f"{INPUT_FOLDER}/{doc}")
         number_of_pages = len(reader.pages)
         doc_name = doc.replace("pdf", "txt")
@@ -295,7 +290,7 @@ def process_document(index, total, doc, INPUT_FOLDER, OUTPUT_FOLDER, BAD_PDF_FOL
         id2doc_list.append((doc, quality))
 
     except Exception as e:
-        logging.error(f"Error processing {doc}: {e}")
+        print(f"Error processing {doc}: {e}")
         sh.move(f"{INPUT_FOLDER}/{doc}", BAD_PDF_FOLDER)
 
 def main():
@@ -341,7 +336,7 @@ def main():
 
     end_time = time.time()  # Record end time
     elapsed_time = end_time - start_time
-    logging.info(f"Total execution time: {elapsed_time:.2f} seconds")
+    print(f"Total execution time: {elapsed_time:.2f} seconds")
 
 if __name__ == "__main__":
     main()
